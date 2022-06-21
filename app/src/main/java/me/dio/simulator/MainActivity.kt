7f84717ffedc9d6db51babe94d8f89968d19d18d
PGoliveira21 <paulooak7@gmail.com>
2022-06-20 22:54:42 -0300

    projeto20/06/2022

--- app/src/main/java/me/dio/simulator/MainActivity.kt
+++ app/src/main/java/me/dio/simulator/MainActivity.kt
@@ -1,11 +1,19 @@
 package me.dio.simulator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import me.dio.simulator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        //val tvHello = findViewById<TextView>(R.id.tvHello)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
